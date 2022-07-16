739e39652efe6a50bd7a784a3c25cef862421697
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-16 20:23:07 +0200

    Add the divisibleBy function The divisibleBy function returns the third parameter if the first parameter is divisible by the second, otherwise returns an empty string

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let input
 do {
-   input = parseInt(prompt(`Insert a number`,15));
-   if (isNaN(input)) {alert(input+` is not a number`)}
+  input = parseInt(prompt(`Insert a number`,20));
+  if (isNaN(input)) {alert(`${input} is not a number`)}
 } while (isNaN(input))
+
+function divisibleBy(dividend,divisor,message) {
+  if (dividend%divisor==0) {return message};
+  return ''
+}
